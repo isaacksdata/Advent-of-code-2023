2cd10da4190eb4212cbf2c8196777a11ce495dec
isaacpersonal <isaacks123@gmail.com>
2023-12-08 09:45:09 +0000

    adding day 1 solution

--- solutions/utilities.py
+++ solutions/utilities.py
@@ -1,7 +1,9 @@
 import os
+from typing import List
 
+from aocd import get_data
+from aocd import submit
 from dotenv import load_dotenv
-from aocd import get_data, submit
 
 
 load_dotenv()
@@ -25,25 +27,45 @@ def get_puzzle(year: int, day: int) -> str:
     return get_data(session=get_session(), day=day, year=year)
 
 
-def submit_answer(answer, part: str, day: int, year: int):
+def submit_answer(answer: int, part: str, day: int, year: int) -> None:
     """
     Submit and answer to AOC
     :param answer: Answer to the problem
     :param part: part of the problem e.g. A or B
     :param day: The day of the problem
     :param year: The year of the problem
-    :return:
+    :return: void
     """
     submit(answer, part=part, day=day, year=year, session=get_session())
 
 
-def read_sample_data(path: str):
+def save_sample_data(data: List[str], path: str) -> None:
+    """
+    Save some sample data to file
+    :param data: data to save to txt file
+    :param path: path to save the data to
+    :return: void
+    """
+    with open(path, "w", encoding="utf-8") as file:
+        file.writelines("\n".join(data))
+
+
+def read_sample_data(path: str) -> List[str]:
     """
     Read some sample data for testing with
     :param path: path to sample data
     :return: data from file
     """
-    with open(path, 'r') as file:
+    with open(path, "r", encoding="utf-8") as file:
         data = file.readlines()
     data = [x.replace("\n", "") for x in data]
     return data
+
+
+def format_input_data(data: str) -> List[str]:
+    """
+    Data is normally returned by API as single string. This function splits into lines
+    :param data: input data string
+    :return: data separated into lines
+    """
+    return data.splitlines()
