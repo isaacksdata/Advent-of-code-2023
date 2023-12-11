f7962c6a84914e52706d116c40fa33140ae58446
isaacpersonal <isaacks123@gmail.com>
2023-12-11 16:00:25 +0000

    solution for day 4 part b

--- solutions/day_4.py
+++ solutions/day_4.py
@@ -1,4 +1,16 @@
+from typing import Dict
 from typing import List
+
+import tqdm
+
+
+def get_card_number(card: str) -> str:
+    """
+    Extract card number from the card string
+    :param card: card string
+    :return: the card number in form of "Card  x"
+    """
+    return card.split(": ")[0]
 
 
 def get_winning_numbers(card: str) -> List[int]:
@@ -48,6 +60,59 @@ def score_cards(data: List[str]) -> int:
     return total
 
 
+def get_card_id(card: str) -> int:
+    """
+    Get just the numeric card ID from the card string
+    :param card: card string
+    :return: card ID
+    """
+    card_id = get_card_number(card)
+    return int(card_id.split("Card ")[1])
+
+
+cache: Dict[int, int] = {}  # used for caching totals per card ID
+
+
+def analyse_card(card: str, original_cards: List[str]) -> int:
+    """
+    Recursive function for getting total cards per original starter card
+
+    This funtion uses the cache dictionary to store the results of each card so each card only has to be
+    analysed once
+    :param card: card string
+    :param original_cards: the original cards list
+    :return: total number of cards receievd due to input card
+    """
+    total = 0
+    current_idx = get_card_id(card)
+    if current_idx in cache:
+        return cache[current_idx]
+    n = get_matches(get_winning_numbers(card), get_trial_numbers(card))
+    total += n
+    if n > 0:
+        new_cards = [
+            c for c in original_cards if any([get_card_id(c) == i for i in range(current_idx + 1, current_idx + 1 + n)])
+        ]
+        for new_card in new_cards:
+            total += analyse_card(new_card, original_cards)
+    cache[current_idx] = total
+    return total
+
+
+def count_cards(data: List[str]) -> int:
+    """
+    Count the total number of cards received
+    :param data: input starter cards
+    :return: total number of cards
+    """
+    original_cards = data.copy()
+    totals = []
+    for card in tqdm.tqdm(data):
+        card_total = analyse_card(card, original_cards)
+        totals.append(card_total)
+    return sum(totals) + len(data)
+
+
 def solve(data: List[str], part: str = "a") -> int:
     """
     Solve the problem for day 1
@@ -58,4 +123,4 @@ def solve(data: List[str], part: str = "a") -> int:
     if part == "a":
         return score_cards(data)
     else:
-        return 1
+        return count_cards(data)
